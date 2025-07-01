2410930c35405eda87198454195cdc195e4d67c9
maryanne <kaffermaryanne@gmail.com>
2025-06-30 21:29:12 -0300

    .

--- q4_listas.py
+++ q4_listas.py
@@ -1,12 +1,12 @@
 lista_funcionarios = []
 id_global = 5251073
 
-def decoração(titulo, funcao): # Decorador
-    lenght = 20 + len(titulo)
+def decoração(titulo, funcao): # Decoração do menu
+    tamanho = 20 - int(len(titulo) / 2)
     print()
-    print("-" * 20 + titulo + "-" * 20)
+    print("-" * tamanho + titulo + "-" * tamanho)
     funcao()
-    print(lenght * 2 * "-")
+    print(40 * "-")
     print()
 
 def cadastrar_funcionario(id): # cadastra funcionario
@@ -15,35 +15,35 @@ def cadastrar_funcionario(id): # cadastra funcionario
     salario = float(input("Digite o salário do funcionário: "))
     lista = {"id": id, "nome": nome, "setor": setor, "salario": salario} # Criação do dicionário
     lista_funcionarios.append(lista.copy()) # Adicionando o dicionário na lista
-    return id
 
 def consultar_funcionarios(): # consulta funcionarios
     while True:
         try:
-            resposta = int(input("1 - Consultar Todos\n2 - Consultar por Id\n3 - Consultar por Setor\n4 - Retornar\nResposta: "))
+            resposta = int(input("1 - Consultar Todos\n2 - Consultar por Id\n3 - Consultar por Setor\n4 - Retornar\nResposta: ")) 
             if resposta not in [1, 2, 3, 4]: # Validação
                 print("Opção inexistente")
                 continue
             elif resposta == 1: # Mostrando todos os funcionários
-                print(lista_funcionarios)
+                for funcionario in lista_funcionarios:
+                    print(f"\nId: {funcionario['id']}\nNome: {funcionario['nome']}\nSetor: {funcionario['setor']}\nSalário: R${funcionario['salario']:.2f}\n")
             elif resposta == 2:
                 try:
                     encontrado = False
                     id = int(input("Digite o id do funcionário: "))
                     for funcionario in lista_funcionarios: # Procurando o funcionário por id na lista
                         if funcionario["id"] == id:
-                            print(funcionario)
+                            print(f"\nId: {funcionario['id']}\nNome: {funcionario['nome']}\nSetor: {funcionario['setor']}\nSalário: R${funcionario['salario']:.2f}\n")
                             encontrado = True
                     if not encontrado: # Validação
                         print("Id não encontrado")
-                except ValueError:
+                except ValueError: # Tratamento de erro
                     print("Id inválido")
             elif resposta == 3: # Consulta por setor
                 encontrado = False
                 setor = input("Digite o setor do funcionário: ")
                 for funcionario in lista_funcionarios: # Procurando o setor por funcionário na lista
                     if funcionario["setor"] == setor:
-                        print(funcionario)
+                        print(f"\nId: {funcionario['id']}\nNome: {funcionario['nome']}\nSetor: {funcionario['setor']}\nSalário: R${funcionario['salario']:.2f}\n")
                         encontrado = True
                 if not encontrado: # Validação
                     print("Setor não encontrado")
@@ -72,14 +72,12 @@ def remover_funcionario(): # remove funcionario
             continue
 
 # Programa principal
-print("Hello World")
-
-# Menu
-while True:
+print("-Bem-vindo ao Sistema da Maryanne Kaffer-")
+while True: # Menu
     try:
-        print("-" * 30 + "Menu Principal" + "-" * 30)
+        print("-" * 13 + "Menu Principal" + "-" * 13)
         resposta = int(input("1 - Cadastrar Funcionário\n2 - Consultar Funcionário\n3 - Remover Funcionário\n4 - Encerrar Programa\nResposta: "))
-        print("-" * 60)
+        print("-" * 40)
         if resposta not in [1, 2, 3, 4]: # Validação
             print("Opção inexistente")
             continue
@@ -96,4 +94,3 @@ while True:
     except ValueError: # Tratamento de erro
         print("Opção inválida")
         continue
-
